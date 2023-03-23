4e9505a0a2db23e6749fbed5570142c7745f89d9
Olrio <opm.rio@gmail.com>
2023-03-23 11:02:12 +0100

    Ajout des tests pour la view index de lettings - failed

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,47 @@
-# from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import (
+    Address,
+    Letting
+)
 
-# Create your tests here.
+
+class DataTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.address1 = Address.objects.create(
+            number=200,
+            street='Ocean Street',
+            city='New York',
+            state='USA',
+            country_iso_code='NY',
+            zip_code=99,
+        )
+        cls.address2 = Address.objects.create(
+            number=55,
+            street='Avenue du Général De Gaulle',
+            city='Strasbourg',
+            state='France',
+            country_iso_code='Alsace',
+            zip_code=57,
+        )
+        cls.letting1 = Letting.objects.create(
+            address=cls.address1,
+            title='Home Sweet Home',
+        )
+        cls.letting2 = Letting.objects.create(
+            address=cls.address2,
+            title='Bienvenue en Alsace',
+        )
+
+
+class TestLettings(DataTest):
+    def test_index(self):
+        self.client = Client()
+        url = reverse("lettings")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        self.assertContains(response, '<title>Lettings</title>')
+        self.assertContains(response, self.letting1.title)
+        self.assertContains(response, self.letting2.title)
+
