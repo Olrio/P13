b6a48d2b2f48738f7eb84abc952f4656ab977464
Olrio <opm.rio@gmail.com>
2023-03-24 20:30:16 +0100

    Ajout du test de la view index de oc_lettings_site

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,14 @@
-def test_dummy():
-    assert 1
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class TestSite(TestCase):
+    def test_index(self):
+        self.client = Client()
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<title>Holiday Homes</title>')
+        self.assertContains(response, f'<a href="{reverse("profiles")}">Profiles</a')
+        self.assertContains(response, f'<a href="{reverse("lettings")}">Lettings</a')
+        self.assertTemplateUsed(response, "oc_lettings_site/index.html")
